8651fcdcb8160346b4f97a42ebf2262f31e32b85
Jan Hutar <jhutar@redhat.com>
2016-11-21 10:26:55 +0100

    Replaces content-convenient.sh with playbook

---
+++ playbooks/satellite/roles/client-content/files/init.pp
@@ -0,0 +1,9 @@
+class qaredhattest {
+  file { "{{ content_puppet_module_file }}":
+    ensure => file,
+    mode   => 755,
+    owner  => root,
+    group  => root,
+    content => "{{ content_puppet_module_file_content }}",
+  }
+}
