0d53607434655a90316cb03b193304be4a29dd86
Jan Hutar <jhutar@redhat.com>
2018-02-07 08:23:17 +0100

    Puppet4 needs it this way: Failed to apply catalog: Parameter mode failed on File[/tmp/puppet-satperftest.txt]: The file mode specification must be a string, not 'Fixnum' at /etc/puppetlabs/code/environments/KT_Default_Organization_Library_SatPerfContentView_2/modules/satperftest/manifests/init.pp:2

--- playbooks/satellite/roles/client-content/files/init.pp
+++ playbooks/satellite/roles/client-content/files/init.pp
@@ -1,7 +1,7 @@
 class {{ content_puppet_module_name }} {
   file { "{{ content_puppet_module_file }}":
     ensure => file,
-    mode   => 755,
+    mode   => "755",
     owner  => root,
     group  => root,
     content => "{{ content_puppet_module_file_content }}",
