88e14054029b019b7f26ccf11a1f749f0074908b
Jan Hutar <jhutar@redhat.com>
2018-02-13 23:56:47 +0100

    Make sure to always run certain parts of puppet-big-test.yaml and fix second part of run-puppet-workload.sh and some output formatting

--- run-puppet-workload.sh
+++ run-puppet-workload.sh
@@ -22,18 +22,18 @@ function reg_five() {
 function measure() {
     local concurency=$1
     local host_fives=$(( $concurency / 5 ))
-    log "===== Concurency $concurency: $( date ) ====="
+    log "===== Register and apply one with concurency $concurency: $( date ) ====="
 
     a $concurency-backup-used-containers-count.log -m shell -a "cp /root/container-used-count{,.foobarbaz}" docker-hosts
     reg_five $host_fives
     a $concurency-restore-used-containers-count.log -m shell -a "cp /root/container-used-count{.foobarbaz,}" docker-hosts
 
     ap $concurency-RegisterPuppet.log playbooks/tests/puppet-big-test.yaml --tags REGISTER -e "size=$concurency update_used=false"
-    ./reg-average.sh RegisterPuppet $logs/$concurency-RegisterPuppet.log
+    ./reg-average.sh RegisterPuppet $logs/$concurency-RegisterPuppet.log | tail -n 1
     s 300
 
-    ap $concurency-PickupPuppet.log playbooks/tests/puppet-big-test.yaml --tags DEPLOY_SINGLE -e "size=$concurency"
-    ./reg-average.sh PickupPuppet $logs/$concurency-PickupPuppet.log
+    ap $concurency-PickupPuppetOne.log playbooks/tests/puppet-big-test.yaml --tags DEPLOY_SINGLE -e "size=$concurency"
+    ./reg-average.sh PickupPuppet $logs/$concurency-PickupPuppetOne.log | tail -n 1
     s 300
 }
 
@@ -48,21 +48,20 @@ s 300
 
 function measure_lots() {
     local concurency=$1
-    local host_fives=$(( $concurency / 5 ))
-    log "===== Concurency $concurency: $( date ) ====="
-
-    a $concurency-backup-used-containers-count.log -m shell -a "cp /root/container-used-count{,.foobarbaz}" docker-hosts
-    reg_five $host_fives
-    a $concurency-restore-used-containers-count.log -m shell -a "cp /root/container-used-count{.foobarbaz,}" docker-hosts
+    log "===== Apply bunch with concurency $concurency: $( date ) ====="
 
     ap $concurency-PickupPuppetBunch.log playbooks/tests/puppet-big-test.yaml --tags REGISTER,DEPLOY_BUNCH -e "size=$concurency"
-    ./reg-average.sh PickupPuppet $logs/$concurency-PickupPuppetBunch.log
+    ./reg-average.sh PickupPuppet $logs/$concurency-PickupPuppetBunch.log | tail -n 1
     s 300
 }
 
-measure_lots 20
-measure_lots 60
-measure_lots 100
-measure_lots 140
-measure_lots 180
-measure_lots 220
+log "===== Registering hosts for experiment with lots of modules ====="
+reg_five 15   # so we have 15 * 5 = 75 registered containers on each docker host
+a prepare-more-trim-used.log -m shell -a "echo 0 >/root/container-used-count" docker-hosts
+
+measure_lots 2
+measure_lots 6
+measure_lots 10
+measure_lots 14
+measure_lots 18
+measure_lots 22
