d600629b500f99009d524599944e6f5465376c6f
Jan Hutar <jhutar@redhat.com>
2018-02-14 08:05:50 +0100

    Use UTC time

--- run-puppet-workload.sh
+++ run-puppet-workload.sh
@@ -12,7 +12,7 @@ s 300
 
 function reg_five() {
     # Register "$1 * 5 * number_of_docker_hosts" containers
-    d=$( date --iso-8601=seconds )
+    d=$( date --utc --iso-8601=seconds )
     for i in $( seq $1 ); do
         ap reg-$d-$i.log playbooks/tests/registrations.yaml -e "size=5 tags=untagged,REG,REM bootstrap_retries=3"
         s 300
@@ -22,7 +22,7 @@ function reg_five() {
 function measure() {
     local concurency=$1
     local host_fives=$(( $concurency / 5 ))
-    log "===== Register and apply one with concurency $concurency: $( date ) ====="
+    log "===== Register and apply one with concurency $concurency: $( date --utc ) ====="
 
     a $concurency-backup-used-containers-count.log -m shell -a "cp /root/container-used-count{,.foobarbaz}" docker-hosts
     reg_five $host_fives
@@ -48,14 +48,14 @@ s 300
 
 function measure_lots() {
     local concurency=$1
-    log "===== Apply bunch with concurency $concurency: $( date ) ====="
+    log "===== Apply bunch with concurency $concurency: $( date --utc ) ====="
 
     ap $concurency-PickupPuppetBunch.log playbooks/tests/puppet-big-test.yaml --tags REGISTER,DEPLOY_BUNCH -e "size=$concurency"
     ./reg-average.sh PickupPuppet $logs/$concurency-PickupPuppetBunch.log | tail -n 1
     s 300
 }
 
-log "===== Registering hosts for experiment with lots of modules ====="
+log "===== Registering hosts for experiment with lots of modules: $( date --utc ) ====="
 reg_five 15   # so we have 15 * 5 = 75 registered containers on each docker host
 a prepare-more-trim-used.log -m shell -a "echo 0 >/root/container-used-count" docker-hosts
 
