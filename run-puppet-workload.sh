dae1800b1c9f71126712fd94736e1a145661833d
Jan Hutar <jhutar@redhat.com>
2018-04-25 08:49:20 +0200

    Fix few smaller issues with this test

--- run-puppet-workload.sh
+++ run-puppet-workload.sh
@@ -2,22 +2,32 @@
 
 source run-library.sh
 
-opts="--forks 100 -i conf/2018-03-13-puppet4-with-tunings/inventory.ini"
+sleep_time=60
+opts="--forks 100 -i conf/miniperf/inventory.ini"
 opts_adhoc="$opts --user root"
 
+# Checked that all works and then remove "-e" flag so every error do not terminate whole run
+a check-ping-docker.log satellite6 -m "shell" -a "ping -c 3 {{ groups['docker-hosts']|first }}"
+a check-ping-sat.log docker-hosts -m "shell" -a "ping -c 3 {{ groups['satellite6']|first }}"
+a check-hammer-ping.log satellite6 -m "shell" -a "! ( hammer -u admin -p changeme ping | grep 'Status:' | grep -v 'ok$' )"
+a check-sat-content.log satellite6 -m "shell" -a "hammer -u admin -p changeme os info --id 1 | grep 'Family:\s\+Redhat'"
+set +e
+
+# Prepare environment
 ap satellite-remove-hosts.log playbooks/satellite/satellite-remove-hosts.yaml &
-ap docker-tierdown-tierup.log playbooks/docker/docker-tierdown.yaml playbooks/docker/docker-tierup.yaml &
+ap docker-tierdown-tierup.log playbooks/docker/docker-tierdown.yaml playbooks/docker/docker-tierup.yaml playbooks/satellite/client-scripts.yaml &
 a rex-cleanup-know_hosts.log satellite6 -m "shell" -a "rm -rf /usr/share/foreman-proxy/.ssh/known_hosts*" &
 wait
 a satellite-drop-caches.log -m shell -a "katello-service stop; sync; echo 3 > /proc/sys/vm/drop_caches; katello-service start" satellite6
-s 300
+s $sleep_time
 
 function reg_five() {
     # Register "$1 * 5 * number_of_docker_hosts" containers
     d=$( date --utc --iso-8601=seconds )
     for i in $( seq $1 ); do
-        ap reg-$d-$i.log playbooks/tests/registrations.yaml -e "size=5 tags=untagged,REG,REM bootstrap_retries=3 bootstrap_operatingsystem='RHEL Server 7.4' grepper='Register'"
-        s 300
+        ap reg-$d-$i.log playbooks/tests/registrations.yaml -e "size=5 tags=untagged,REG,REM bootstrap_retries=3 grepper='Register'"
+        log "$( ./reg-average.sh Register $logs/reg-$d-$i.log | tail -n 1 )"
+        s $sleep_time
     done
 }
 
@@ -26,17 +36,17 @@ function measure() {
     local host_fives=$(( $concurency / 5 ))
     log "===== Register and apply one with concurency $concurency: $( date --utc ) ====="
 
-    a $concurency-backup-used-containers-count.log -m shell -a "cp /root/container-used-count{,.foobarbaz}" docker-hosts
+    a $concurency-backup-used-containers-count.log -m shell -a "touch /root/container-used-count; cp /root/container-used-count{,.foobarbaz}" docker-hosts
     reg_five $host_fives
     a $concurency-restore-used-containers-count.log -m shell -a "cp /root/container-used-count{.foobarbaz,}" docker-hosts
 
     ap $concurency-RegisterPuppet.log playbooks/tests/puppet-big-test.yaml --tags REGISTER -e "size=$concurency update_used=false"
-    ./reg-average.sh RegisterPuppet $logs/$concurency-RegisterPuppet.log | tail -n 1
-    s 300
+    log "$( ./reg-average.sh RegisterPuppet $logs/$concurency-RegisterPuppet.log | tail -n 1 )"
+    s $sleep_time
 
     ap $concurency-PickupPuppetOne.log playbooks/tests/puppet-big-test.yaml --tags DEPLOY_SINGLE -e "size=$concurency"
-    ./reg-average.sh PickupPuppet $logs/$concurency-PickupPuppetOne.log | tail -n 1
-    s 300
+    log "$( ./reg-average.sh PickupPuppet $logs/$concurency-PickupPuppetOne.log | tail -n 1 )"
+    s $sleep_time
 }
 
 measure 5
@@ -48,18 +58,18 @@ measure 50
 measure 60
 
 ap satellite-remove-hosts.log playbooks/satellite/satellite-remove-hosts.yaml &
-ap docker-tierdown-tierup.log playbooks/docker/docker-tierdown.yaml playbooks/docker/docker-tierup.yaml &
+ap docker-tierdown-tierup.log playbooks/docker/docker-tierdown.yaml playbooks/docker/docker-tierup.yaml playbooks/satellite/client-scripts.yaml &
 a rex-cleanup-know_hosts.log satellite6 -m "shell" -a "rm -rf /usr/share/foreman-proxy/.ssh/known_hosts*" &
 wait
-s 300
+s $sleep_time
 
 function measure_lots() {
     local concurency=$1
     log "===== Apply bunch with concurency $concurency: $( date --utc ) ====="
 
     ap $concurency-PickupPuppetBunch.log playbooks/tests/puppet-big-test.yaml --tags REGISTER,DEPLOY_BUNCH -e "size=$concurency"
-    ./reg-average.sh PickupPuppet $logs/$concurency-PickupPuppetBunch.log | tail -n 1
-    s 300
+    log "$( ./reg-average.sh PickupPuppet $logs/$concurency-PickupPuppetBunch.log | tail -n 1 )"
+    s $sleep_time
 }
 
 log "===== Registering hosts for experiment with lots of modules: $( date --utc ) ====="
