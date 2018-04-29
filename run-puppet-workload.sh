ee4a528d755b06bec0755f57b40cf8147647d93d
Jan Hutar <jhutar@redhat.com>
2018-04-29 09:34:39 +0200

    Bit of cleanup in run-puppet-workload.sh

--- run-puppet-workload.sh
+++ run-puppet-workload.sh
@@ -14,21 +14,25 @@ a check-sat-content.log satellite6 -m "shell" -a "hammer -u admin -p changeme os
 set +e
 
 # Prepare environment
+ap satellite-puppet-big-cv.log playbooks/tests/puppet-big-setup.yaml &
 ap satellite-remove-hosts.log playbooks/satellite/satellite-remove-hosts.yaml &
-ap docker-tierdown-tierup.log playbooks/docker/docker-tierdown.yaml playbooks/docker/docker-tierup.yaml playbooks/satellite/client-scripts.yaml &
+ap docker-tierdown-tierup.log playbooks/docker/docker-tierdown.yaml playbooks/docker/docker-tierup.yaml &
+ap docker-client-scripts.log playbooks/satellite/client-scripts.yaml &
 a rex-cleanup-know_hosts.log satellite6 -m "shell" -a "rm -rf /usr/share/foreman-proxy/.ssh/known_hosts*" &
 wait
 a satellite-drop-caches.log -m shell -a "katello-service stop; sync; echo 3 > /proc/sys/vm/drop_caches; katello-service start" satellite6
 s $sleep_time
 
 function reg_five() {
-    # Register "$1 * 5 * number_of_docker_hosts" containers
+    # Register "$1 * 5 * number_of_docker_hosts" containers, do not change /root/container-used-count on docker hosts
     d=$( date --utc --iso-8601=seconds )
+    a $d-backup-used-containers-count.log -m shell -a "touch /root/container-used-count; cp /root/container-used-count{,.foobarbaz}" docker-hosts
     for i in $( seq $1 ); do
         ap reg-$d-$i.log playbooks/tests/registrations.yaml -e "size=5 tags=untagged,REG,REM bootstrap_retries=3 grepper='Register'"
         log "$( ./reg-average.sh Register $logs/reg-$d-$i.log | tail -n 1 )"
         s $sleep_time
     done
+    a $d-restore-used-containers-count.log -m shell -a "cp /root/container-used-count{.foobarbaz,}" docker-hosts
 }
 
 function measure() {
@@ -36,16 +40,10 @@ function measure() {
     local host_fives=$(( $concurency / 5 ))
     log "===== Register and apply one with concurency $concurency: $( date --utc ) ====="
 
-    a $concurency-backup-used-containers-count.log -m shell -a "touch /root/container-used-count; cp /root/container-used-count{,.foobarbaz}" docker-hosts
     reg_five $host_fives
-    a $concurency-restore-used-containers-count.log -m shell -a "cp /root/container-used-count{.foobarbaz,}" docker-hosts
-
-    ap $concurency-RegisterPuppet.log playbooks/tests/puppet-big-test.yaml --tags REGISTER -e "size=$concurency update_used=false"
-    log "$( ./reg-average.sh RegisterPuppet $logs/$concurency-RegisterPuppet.log | tail -n 1 )"
-    s $sleep_time
-
-    ap $concurency-PickupPuppetOne.log playbooks/tests/puppet-big-test.yaml --tags DEPLOY_SINGLE -e "size=$concurency"
-    log "$( ./reg-average.sh PickupPuppet $logs/$concurency-PickupPuppetOne.log | tail -n 1 )"
+    ap $concurency-PuppetOne.log playbooks/tests/puppet-big-test.yaml --tags REGISTER,DEPLOY_SINGLE -e "size=$concurency"
+    log "$( ./reg-average.sh RegisterPuppet $logs/$concurency-PuppetOne.log | tail -n 1 )"
+    log "$( ./reg-average.sh PickupPuppet $logs/$concurency-PuppetOne.log | tail -n 1 )"
     s $sleep_time
 }
 
@@ -67,14 +65,14 @@ function measure_lots() {
     local concurency=$1
     log "===== Apply bunch with concurency $concurency: $( date --utc ) ====="
 
-    ap $concurency-PickupPuppetBunch.log playbooks/tests/puppet-big-test.yaml --tags REGISTER,DEPLOY_BUNCH -e "size=$concurency"
-    log "$( ./reg-average.sh PickupPuppet $logs/$concurency-PickupPuppetBunch.log | tail -n 1 )"
+    ap $concurency-PuppetBunch.log playbooks/tests/puppet-big-test.yaml --tags REGISTER,DEPLOY_BUNCH -e "size=$concurency"
+    log "$( ./reg-average.sh SetupPuppet $logs/$concurency-PuppetBunch.log | tail -n 1 )"
+    log "$( ./reg-average.sh PickupPuppet $logs/$concurency-PuppetBunch.log | tail -n 1 )"
     s $sleep_time
 }
 
 log "===== Registering hosts for experiment with lots of modules: $( date --utc ) ====="
 reg_five 15   # so we have 15 * 5 = 75 registered containers on each docker host
-a prepare-more-trim-used.log -m shell -a "echo 0 >/root/container-used-count" docker-hosts
 
 measure_lots 2
 measure_lots 6
