c64054fcafb3773fa229795eea5e9e6603656417
Jan Hutar <jhutar@redhat.com>
2019-12-25 09:10:27 +0100

    run-test.sh: Not needed for a quick test

--- run-test.sh
+++ run-test.sh
@@ -34,10 +34,10 @@ a 00-info-rpm-qa.log satellite6 -m "shell" -a "rpm -qa | sort"
 a 00-info-hostname.log satellite6 -m "shell" -a "hostname"
 a 00-check-ping-sat.log docker-hosts -m "shell" -a "ping -c 3 {{ groups['satellite6']|first }}"
 a 00-check-hammer-ping.log satellite6 -m "shell" -a "! ( hammer $hammer_opts ping | grep 'Status:' | grep -v 'ok$' )"
-ap 00-recreate-containers.log playbooks/docker/docker-tierdown.yaml playbooks/docker/docker-tierup.yaml
-ap 00-recreate-client-scripts.log playbooks/satellite/client-scripts.yaml
-ap 00-remove-hosts-if-any.log playbooks/satellite/satellite-remove-hosts.yaml
-a 00-satellite-drop-caches.log -m shell -a "katello-service stop; sync; echo 3 > /proc/sys/vm/drop_caches; katello-service start" satellite6
+#ap 00-recreate-containers.log playbooks/docker/docker-tierdown.yaml playbooks/docker/docker-tierup.yaml
+#ap 00-recreate-client-scripts.log playbooks/satellite/client-scripts.yaml
+#ap 00-remove-hosts-if-any.log playbooks/satellite/satellite-remove-hosts.yaml
+#a 00-satellite-drop-caches.log -m shell -a "katello-service stop; sync; echo 3 > /proc/sys/vm/drop_caches; katello-service start" satellite6
 a 00-info-rpm-q-satellite.log satellite6 -m "shell" -a "rpm -q satellite"
 satellite_version=$( tail -n 1 $logs/00-info-rpm-q-satellite.log ); echo "$satellite_version" | grep '^satellite-6\.'   # make sure it was detected correctly
 
