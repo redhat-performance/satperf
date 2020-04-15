c4a187145c961c29924861a3ec7400807ff26c70
Jan Hutar <jhutar@redhat.com>
2020-04-15 22:14:41 +0200

    scripts/get-task-fuzzy-duration.py: Reorganize code, WIP

--- scripts/get-task-fuzzy-duration.py
+++ scripts/get-task-fuzzy-duration.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+#!/usr/bin/env python3
 
 import logging
 import sys
@@ -7,30 +6,17 @@ import datetime
 import dateutil.parser
 import collections
 import requests
+import argparse
+import urllib3
 import simplejson.scanner
-
-username = sys.argv[1]
-password = sys.argv[2]
-server = sys.argv[3]
-task_id = sys.argv[4]
-
-# Ignore time when less then this percentage of jobs were running
-# E.g. When we have job with 100 sub-tasks, percentage 5 would ignore
-# time from beginning and from end when less than 5 sub-tasks were
-# running (so 3 stucked sub-cases wont affect conted duration of
-# the main task)
-if len(sys.argv) == 6:
-    percentage = float(sys.argv[5])
-else:
-    percentage = 3
 
 per_page = 100
 datetime_fmt = "%Y-%m-%d %H:%M"
 
 
-def get_json(uri, params=None):
+def get_json(hostname, uri, username, password, params=None):
     r = requests.get(
-            "https://%s%s" % (server, uri),
+            "https://%s%s" % (hostname, uri),
             params=params,
             auth=(username, password),
             verify=False)
@@ -41,7 +27,7 @@ def get_json(uri, params=None):
         sys.exit(1)
 
 
-def get_all(uri, params=None):
+def get_all(hostname, uri, username, password, params=None):
     out = []
     if params is None:
         params = {'per_page': per_page}
@@ -50,60 +36,103 @@ def get_all(uri, params=None):
     page = 1
     while True:
         params['page'] = page
-        r = get_json(uri, params)
+        r = get_json(hostname, uri, username, password, params)
         out += r['results']
         if int(r['page']) * int(r['per_page']) >= int(r['total']):
             return out
         page += 1
 
 
-parent_task = get_json("/foreman_tasks/api/tasks/%s" % task_id)
-if 'error' in parent_task:
-    logging.error("Error retrieving parent task info: %s" % parent_task)
-    sys.exit(1)
-if 'state' not in parent_task:
-    logging.error("Unexpected parent task format, missing 'state': %s"
-                  % parent_task)
-    sys.exit(1)
-if parent_task['state'] != 'stopped':
-    logging.error("Parent task not finished yet: %s" % parent_task)
-    sys.exit(1)
+def investigate_task(args):
+    parent_task = get_json(
+        args.hostname, "/foreman_tasks/api/tasks/%s" % args.task_id,
+        args.username, args.password)
+    if 'error' in parent_task:
+        logging.error("Error retrieving parent task info: %s" % parent_task)
+        sys.exit(1)
+    if 'state' not in parent_task:
+        logging.error("Unexpected parent task format, missing 'state': %s"
+                      % parent_task)
+        sys.exit(1)
+    if parent_task['state'] != 'stopped':
+        logging.error("Parent task not finished yet: %s" % parent_task)
+        sys.exit(1)
 
-sub_tasks = get_all("/foreman_tasks/api/tasks",
-                    {"search": "parent_task_id = %s" % task_id})
-# with open('cache', 'w') as fp:
-#     import json
-#     json.dump(sub_tasks, fp)
-# with open('cache', 'r') as fp:
-#     import json
-#     sub_tasks = json.load(fp)
+    sub_tasks = get_all(
+        args.hostname, "/foreman_tasks/api/tasks",
+        args.username, args.password,
+        {"search": "parent_task_id = %s" % args.task_id})
+    # with open('cache', 'w') as fp:
+    #     import json
+    #     json.dump(sub_tasks, fp)
+    # with open('cache', 'r') as fp:
+    #     import json
+    #     sub_tasks = json.load(fp)
 
-logging.debug("Task %s have %s sub-tasks" % (task_id, len(sub_tasks)))
-count = 0
-data = {}
-minute = datetime.timedelta(0, 60, 0)
-for t in sub_tasks:
-    count += 1
-    s = dateutil.parser.parse(t['started_at'])
-    e = dateutil.parser.parse(t['ended_at'])
-    key = s
-    while key <= e:
-        key_str = key.strftime(datetime_fmt)
-        if key_str not in data:
-            data[key_str] = 0
-        data[key_str] += 1
-        key += minute
-data = collections.OrderedDict(sorted(data.items(), key=lambda t: t[0]))
+    logging.debug("Task %s have %s sub-tasks" % (args.task_id, len(sub_tasks)))
+    count = 0
+    data = {}
+    minute = datetime.timedelta(0, 60, 0)
+    for t in sub_tasks:
+        count += 1
+        s = dateutil.parser.parse(t['started_at'])
+        e = dateutil.parser.parse(t['ended_at'])
+        key = s
+        while key <= e:
+            key_str = key.strftime(datetime_fmt)
+            if key_str not in data:
+                data[key_str] = 0
+            data[key_str] += 1
+            key += minute
+    data = collections.OrderedDict(sorted(data.items(), key=lambda t: t[0]))
 
-max_val = float(count) * percentage / 100
-start = None
-for key, val in data.items():
-    if val > max_val:
-        start = dateutil.parser.parse(key)
-        break
-end = None
-for key, val in reversed(data.items()):
-    if val > max_val:
-        end = dateutil.parser.parse(key)
-        break
-print("When removed start and end of task with less than %.2f of running tasks, task started at %s, finished at %s and lasted for %s" % (max_val, start, end, end - start))   # noqa: E501
+    max_val = float(count) * args.percentage / 100
+    start = None
+    for key, val in data.items():
+        if val > max_val:
+            start = dateutil.parser.parse(key)
+            break
+    end = None
+    for key, val in reversed(data.items()):
+        if val > max_val:
+            end = dateutil.parser.parse(key)
+            break
+    print("When removed start and end of task with less than %.2f of running tasks, task started at %s, finished at %s and lasted for %s" % (max_val, start, end, end - start))   # noqa: E501
+
+
+def doit():
+    parser = argparse.ArgumentParser(
+        description='Show task duration with given percentage of hanged sub-tasks removed from the equation',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument('-u', '--username', default='admin',
+                        help='Satellite username')
+    parser.add_argument('-p', '--password', default='changeme',
+                        help='Satellite password')
+    parser.add_argument('--hostname', required=True,
+                        help='Satellite hostname')
+    parser.add_argument('--task-id', required=True,
+                        help='Task ID you want to investigate')
+    parser.add_argument('--percentage', type=float, default=3,
+                        help='How many % of longest sub-tasks to ignore')
+    parser.add_argument('--cache',
+                        help='Cache data to this file, do not cache when empty')
+    parser.add_argument('--dont-hide-warnings', action='store_true',
+                        help='Show urllib3 warnings like InsecureRequestWarning')
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='Show debug output')
+    args = parser.parse_args()
+
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+
+    if not args.dont_hide_warnings:
+        urllib3.disable_warnings()
+
+    logging.debug("Args: {args}")
+
+    return investigate_task(args)
+
+
+if __name__ == '__main__':
+    sys.exit(doit())
