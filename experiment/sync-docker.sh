b683317e0bab2300abca9995a0bf695d76b1d9f1
Imaanpreet Kaur <ikaur@redhat.com>
2020-05-18 15:56:38 +0530

    changes in sync process via iso and docker

--- experiment/sync-docker.sh
+++ experiment/sync-docker.sh
@@ -6,13 +6,12 @@ manifest="${PARAM_manifest:-conf/contperf/manifest.zip}"
 inventory="${PARAM_inventory:-conf/contperf/inventory.ini}"
 private_key="${PARAM_private_key:-conf/contperf/id_rsa_perf}"
 
-
 wait_interval=${PARAM_wait_interval:-50}
 
 cdn_url_mirror="${PARAM_cdn_url_mirror:-https://cdn.redhat.com/}"
 cdn_url_full="${PARAM_cdn_url_full:-https://cdn.redhat.com/}"
 
-PARAM_iso_repos=${PARAM_iso_repos:-http://storage.example.com/iso-repos/}
+PARAM_docker_registry=${PARAM_docker_registry:-https://registry-1.docker.io/}
 
 do="Default Organization"
 dl="Default Location"
@@ -20,11 +19,12 @@ dl="Default Location"
 opts="--forks 100 -i $inventory --private-key $private_key"
 opts_adhoc="$opts --user root -e @conf/satperf.yaml -e @conf/satperf.local.yaml"
 
+
 section "Checking environment"
 generic_environment_check
 
 section "Sync file repo"
-ap 10-test-sync-repositories.log playbooks/tests/sync-iso.yaml -e "test_sync_repositories_count=$test_sync_repositories_count test_sync_repositories_url_template=$test_sync_repositories_url_template test_sync_repositories_max_sync_secs=$test_sync_repositories_max_sync_secs"
+ap 10-test-sync-repositories.log playbooks/tests/sync-docker.yaml -e "test_sync_repositories_count=$test_sync_repositories_count test_sync_repositories_url_template=$test_sync_repositories_url_template test_sync_repositories_max_sync_secs=$test_sync_repositories_max_sync_secs"
 
 section "Summary"
 e SyncRepositories $logs/10-test-sync-repositories.log
@@ -32,4 +32,3 @@ e PublishContentViews $logs/10-test-sync-repositories.log
 e PromoteContentViews $logs/10-test-sync-repositories.log
 
 junit_upload
-~             
