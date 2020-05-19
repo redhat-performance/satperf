ab95ff773f17586281ebb08d8c8d4c1a258ee5f3
Imaanpreet Kaur <ikaur@redhat.com>
2020-05-19 12:45:34 +0530

    Added the values of export parameters

--- experiment/sync-docker.sh
+++ experiment/sync-docker.sh
@@ -6,6 +6,10 @@ manifest="${PARAM_manifest:-conf/contperf/manifest.zip}"
 inventory="${PARAM_inventory:-conf/contperf/inventory.ini}"
 private_key="${PARAM_private_key:-conf/contperf/id_rsa_perf}"
 
+test_sync_repositories_count="${PARAM_test_sync_repositories_count:-8}"
+test_sync_repositories_url_template="${PARAM_test_sync_repositories_url_template:-https://registry-1.docker.io*}"
+test_sync_repositories_max_sync_secs="${PARAM_test_sync_repositories_max_sync_secs:-600}"
+
 wait_interval=${PARAM_wait_interval:-50}
 
 cdn_url_mirror="${PARAM_cdn_url_mirror:-https://cdn.redhat.com/}"
