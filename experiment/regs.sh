aa7016f0a5816fc34b0fb2d3ba4e0c29390e7b11
Jan Hutar <jhutar@redhat.com>
2020-06-04 09:14:00 +0200

    experiment/regs.sh: Backport current way of doing stuff from newer experiment files

--- experiment/regs.sh
+++ experiment/regs.sh
@@ -17,24 +17,11 @@ do="Default Organization"
 dl="Default Location"
 
 opts="--forks 100 -i $inventory --private-key $private_key"
-opts_adhoc="$opts --user root"
+opts_adhoc="$opts --user root -e @conf/satperf.yaml -e @conf/satperf.local.yaml"
 
 
 section "Checking environment"
-a regs-00-info-rpm-qa.log satellite6 -m "shell" -a "rpm -qa | sort"
-a regs-00-info-hostname.log satellite6 -m "shell" -a "hostname"
-a regs-00-check-ping-sat.log docker-hosts -m "shell" -a "ping -c 3 {{ groups['satellite6']|first }}"
-a regs-00-check-hammer-ping.log satellite6 -m "shell" -a "! ( hammer $hammer_opts ping | grep 'Status:' | grep -v 'ok$' )"
-ap regs-00-recreate-containers.log playbooks/docker/docker-tierdown.yaml playbooks/docker/docker-tierup.yaml
-ap regs-00-recreate-client-scripts.log playbooks/satellite/client-scripts.yaml
-ap regs-00-remove-hosts-if-any.log playbooks/satellite/satellite-remove-hosts.yaml
-a regs-00-satellite-drop-caches.log -m shell -a "katello-service stop; sync; echo 3 > /proc/sys/vm/drop_caches; katello-service start" satellite6
-a 00-info-rpm-q-katello.log satellite6 -m "shell" -a "rpm -q katello"
-katello_version=$( tail -n 1 $logs/00-info-rpm-q-katello.log ); echo "$katello_version" | grep '^katello-[0-9]\.'   # make sure it was detected correctly
-a 00-info-rpm-q-satellite.log satellite6 -m "shell" -a "rpm -q satellite || true"
-satellite_version=$( tail -n 1 $logs/00-info-rpm-q-satellite.log )
-s $( expr 3 \* $wait_interval )
-set +e
+generic_environment_check
 
 
 section "Upload manifest"
