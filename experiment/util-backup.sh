61cd05844b1f47b87dfa2af6250ab9c29ff7e60d
Jan Hutar <jhutar@redhat.com>
2020-07-03 21:01:38 +0200

    experiment/util-backup.sh experiment/util-restore.sh: Should resolve 'ERROR: Test reports were found but none of them are new. Did leafNodes run?'

--- experiment/util-backup.sh
+++ experiment/util-backup.sh
@@ -14,3 +14,6 @@ opts_adhoc="$opts --user root -e @conf/satperf.yaml -e @conf/satperf.local.yaml"
 section "Backup"
 a 00-backup.log satellite6 -m "shell" -a "rm -rf /root/backup /tmp/backup; mkdir /tmp/backup; satellite-maintain backup offline --skip-pulp-content --assumeyes /tmp/backup; mv /tmp/backup /root/"
 a 00-hammer-ping.log satellite6 -m "shell" -a "hammer -u {{ sat_user }} -p {{ sat_pass }} ping"
+
+
+junit_upload
--- experiment/util-restore.sh
+++ experiment/util-restore.sh
@@ -14,3 +14,6 @@ opts_adhoc="$opts --user root -e @conf/satperf.yaml -e @conf/satperf.local.yaml"
 section "Restore"
 a 00-backup.log satellite6 -m "shell" -a "rm -rf /tmp/backup; cp -r /root/backup /tmp/; satellite-maintain restore --assumeyes /tmp/backup/*; rm -rf /tmp/backup"
 a 00-hammer-ping.log satellite6 -m "shell" -a "hammer -u {{ sat_user }} -p {{ sat_pass }} ping"
+
+
+junit_upload
