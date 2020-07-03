6e4bc32359c4f7a83f919f40018710a6d76f608c
Jan Hutar <jhutar@redhat.com>
2020-07-03 15:21:50 +0200

    experiment/util-backup.sh: Also add basic sanity check

--- experiment/util-backup.sh
+++ experiment/util-backup.sh
@@ -18,3 +18,4 @@ s $wait_interval
 
 section "Backup"
 a 00-backup.log satellite6 -m "shell" -a "rm -rf /root/backup /tmp/backup; mkdir /tmp/backup; satellite-maintain backup offline --skip-pulp-content /tmp/backup; mv /tmp/backup /root/"
+a 00-hammer-ping.log satellite6 -m "shell" -a "hammer -u {{ sat_user }} -p {{ sat_pass }} ping"
