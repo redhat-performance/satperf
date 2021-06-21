8588541cd3622c76672280948b7e3ee6dbe14f1c
Jan Hutar <jhutar@redhat.com>
2021-06-21 09:11:10 +0200

    infra/kibana/saved_objects/: New Kibana uses ndjson export format

--- infra/kibana/saved_objects/split-export.py
+++ infra/kibana/saved_objects/split-export.py
@@ -2,16 +2,16 @@
 
 """
 This tool is supposed to help with splitting saved objects export
-json file to individual files, so it is easier to manage in git.
+ndjson file to individual files, so it is easier to manage in git.
 
 To get the export from Elastic Search:
 
-    Kibana -> Management -> Saved Objects -> Export X objects
+    Kibana -> Stack Management -> Saved Objects -> fileter for objects you are interested in -> Export X objects
 
 To split it:
 
-    $ ./split-export.py export.json
-    $ rm export.json
+    $ ./split-export.py export.ndjson
+    $ rm export.ndjson
     $ git add *.json
     $ git commit -m "New version of Kibana saved objects"
 """
@@ -24,11 +24,14 @@ export_file = sys.argv[1]
 
 print(f"Loading {export_file}")
 with open(export_file, 'r') as export_fp:
-    print(f"Parsing {export_file}")
-    export_data = json.load(export_fp)
-    for entity in export_data:
-        output_file = f"{entity['_type']}_{entity['_id']}.json"
-        print(f"Writing {output_file}")
-        with open(output_file, 'w') as output_fp:
-            print(f"Dumping {output_file}")
-            json.dump([entity], output_fp, sort_keys=True, indent=4, separators=(',', ': '))
+    for row in export_fp:
+        entity = json.loads(row)
+        if 'exportedCount' in entity:
+            pass   # this is just summary of the export like '{"exportedCount": 41, "missingRefCount": 0, "missingReferences": []}'
+        elif 'type' in entity and 'id' in entity:
+            output_file = f"{entity['type']}_{entity['id']}.json"
+            print(f"Writing {output_file}")
+            with open(output_file, 'w') as output_fp:
+                json.dump([entity], output_fp, sort_keys=True, indent=4, separators=(',', ': '))
+        else:
+            raise Exception(f"Unknown document: {entity}")
