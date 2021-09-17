d5a66022c5a46dce898dd43747583112619e869a
Jan Hutar <jhutar@redhat.com>
2021-09-17 15:43:23 +0200

    Scripts for a setup with lot of duplicate content

---
+++ scripts/create-big-setup-update.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+set -ex
+
+function log() {
+    echo "$( date --utc -Ins ) $@" | tee -a /root/create-big-setup-update-${ORG}.log >&2
+}
+
+function hammer_logged() {
+    local before=$( date +%s )
+    hammer "$@"
+    rc=$?
+    local after=$( date +%s )
+    log "DEBUG Command 'hammer $@' finished in $( expr $after - $before ) seconds with exit code $rc"
+}
+
+function get_id() {
+    hammer --output csv --no-headers repository list --organization ${ORG} | grep "^[0-9]\+,$1," | cut -d ',' -f 1
+}
+
+for ORG in org1 org2 org3 org4 org5; do
+
+rhel7=$( get_id "Red Hat Enterprise Linux 7 Server RPMs x86_64 7Server" )
+rhel7_ansible=$( get_id "Red Hat Ansible Engine 2.9 RPMs for Red Hat Enterprise Linux 7 Server x86_64" )
+rhel7_sattools=$( get_id "Red Hat Satellite Tools 6.9 for RHEL 7 Server RPMs x86_64" )
+rhel7_scl=$( get_id "Red Hat Software Collections RPMs for Red Hat Enterprise Linux 7 Server x86_64 7Server" )
+rhel8_ansible=$( get_id "Red Hat Ansible Engine 2.9 for RHEL 8 x86_64 RPMs" )
+rhel8_appstream=$( get_id "Red Hat Enterprise Linux 8 for x86_64 - AppStream RPMs 8" )
+rhel8_baseos=$( get_id "Red Hat Enterprise Linux 8 for x86_64 - BaseOS RPMs 8" )
+rhel8_sattools=$( get_id "Red Hat Satellite Tools 6.9 for RHEL 8 x86_64 RPMs" )
+rhel8_supp=$( get_id "Red Hat Enterprise Linux 8 for x86_64 - Supplementary RPMs 8" )
+
+for r in $rhel7 $rhel7_ansible $rhel7_sattools $rhel7_scl $rhel8_ansible $rhel8_appstream $rhel8_baseos $rhel8_sattools $rhel8_supp; do
+    hammer_logged repository synchronize --id $r --organization ${ORG} &
+done
+wait
+
+for cv in ${ORG}-cv-rhel7 ${ORG}-cv-rhel7_ansible ${ORG}-cv-rhel7_sattools ${ORG}-cv-rhel7_scl ${ORG}-cv-rhel8_ansible ${ORG}-cv-rhel8_appstream ${ORG}-cv-rhel8_baseos ${ORG}-cv-rhel8_sattools ${ORG}-cv-rhel8_supp; do
+    hammer_logged content-view publish --name $cv --organization ${ORG} &
+done
+wait
+
+for ccv in ${ORG}-ccv-rhel7-min ${ORG}-ccv-rhel7-max ${ORG}-ccv-rhel8-min ${ORG}-ccv-rhel8-max; do
+    hammer_logged content-view publish --name $ccv --organization ${ORG} &
+done
+wait
+
+for from_to in "${ORG}-le2 ${ORG}-le3" "${ORG}-le1 ${ORG}-le2" "Library ${ORG}-le1"; do
+    from=$( echo "$from_to" | cut -d ' ' -f 1 )
+    to=$( echo "$from_to" | cut -d ' ' -f 2 )
+    for ccv in ${ORG}-ccv-rhel7-min ${ORG}-ccv-rhel7-max ${ORG}-ccv-rhel8-min ${ORG}-ccv-rhel8-max; do
+        hammer_logged content-view version promote --content-view $ccv --from-lifecycle-environment $from --to-lifecycle-environment $to --organization ${ORG} &
+    done
+    wait
+done
+
+done
