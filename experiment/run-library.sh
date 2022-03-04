f1f07ff09a3ded5caf5846bc7774760d23cf9c39
Ishan Singhal <isinghal@redhat.com>
2022-03-04 17:47:18 +0530

    AnsibleUndefinedVariable: 'content_activationkey' is undefined

--- experiment/run-library.sh
+++ experiment/run-library.sh
@@ -98,7 +98,7 @@ function generic_environment_check() {
         else
             skip_measurement='true' ap 00-recreate-containers.log playbooks/docker/docker-tierdown.yaml playbooks/docker/docker-tierup.yaml
         fi
-        skip_measurement='true' ap 00-recreate-client-scripts.log playbooks/satellite/client-scripts.yaml
+        skip_measurement='true' ap 00-recreate-client-scripts.log playbooks/satellite/client-scripts.yaml -e content_activationkey=ActivationKey
         skip_measurement='true' ap 00-remove-hosts-if-any.log playbooks/satellite/satellite-remove-hosts.yaml
     fi
 
