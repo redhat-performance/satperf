1595656c76b4e52d487e3bc69884d4a2606101de
Jan Hutar <jhutar@redhat.com>
2022-03-21 08:56:32 +0100

    experiment/full.sh: Fixing Missing arguments for '--group-parameters-attributes'.

--- experiment/full.sh
+++ experiment/full.sh
@@ -180,7 +180,7 @@ for row in $( cut -d ' ' -f 1 $tmp ); do
     subnet_id=$( h_out "--output yaml subnet info --name '$subnet_name'" | grep '^Id:' | cut -d ' ' -f 2 )
     skip_measurement='true' a 45-subnet-add-rex-capsule-$capsule_name.log satellite6 -m "shell" -a "curl --silent --insecure -u {{ sat_user }}:{{ sat_pass }} -X PUT -H 'Accept: application/json' -H 'Content-Type: application/json' https://localhost//api/v2/subnets/$subnet_id -d '{\"subnet\": {\"remote_execution_proxy_ids\": [\"$capsule_id\"]}}'"
     h_out "--no-headers --csv hostgroup list --search 'name = $hostgroup_name'" | grep --quiet '^[0-9]\+,' \
-        || skip_measurement='true' h 41-hostgroup-create-$capsule_name.log "hostgroup create --content-view '$do View' --lifecycle-environment Library --name '$hostgroup_name' --query-organization '$do' --subnet '$subnet_name' --group-parameters-attributes 'name=kt_activation_keys,value=ActivationKey'"
+        || skip_measurement='true' h 41-hostgroup-create-$capsule_name.log "hostgroup create --content-view '$do View' --lifecycle-environment Library --name '$hostgroup_name' --query-organization '$do' --subnet '$subnet_name' --group-parameters-attributes 'name=kt_activation_keys\,value=ActivationKey'"
 done
 
 
