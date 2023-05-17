0ce7e5fb9e6e1f77b60543c233da661c6f1feac5
Imaan <32102000+Imaanpreet@users.noreply.github.com>
2023-05-17 14:15:08 +0530

    Create rex-mqtt.sh
    new file added for performing rex with more load

---
+++ experiment/rex-mqtt.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+source experiment/run-library.sh
+
+organization="${PARAM_organization:-Default Organization}"
+inventory="${PARAM_inventory:-conf/contperf/inventory.ini}"
+local_conf="${PARAM_local_conf:-conf/satperf.local.yaml}"
+
+wait_interval=${PARAM_wait_interval:-50}
+
+dl="Default Location"
+
+opts="--forks 100 -i $inventory"
+opts_adhoc="$opts -e @conf/satperf.yaml -e @$local_conf"
+
+
+section "Checking environment"
+generic_environment_check false false
+
+section "Remote execution"
+skip_measurement='true' h 10-rex-set-via-ip.log "settings set --name remote_execution_connect_by_ip --value true"
+skip_measurement='true' a 11-rex-cleanup-know_hosts.log satellite6 -m "shell" -a "rm -rf /usr/share/foreman-proxy/.ssh/known_hosts*"
+job_template_ssh_default='Run Command - Script Default'
+skip_measurement='true' h 12-rex-sm-facts-update.log "job-invocation create --async --description-format 'Run %{command} (%{template_name})' --inputs command='subscription-manager refresh yum -y install insights-client insights-client --register' --job-template '$job_template_ssh_default' --search-query 'name ~ container'"
+j $logs/12-rex-sm-facts-update.log
+s $wait_interval
+skip_measurement='true' h 13-rex-date.log skip_measurement='true' h 13-rex-date.log "job-invocation create --async --description-format 'Run %{command} (%{template_name})' --inputs command='date' --job-template '$job_template_ssh_default' --search-query 'name ~ container'"
+j $logs/13-rex-date.log
+s $wait_interval
+junit_upload
