183c93f674c553c92941740bf7668fefd8c76a3f
Pablo Méndez Hernández <pablomh@redhat.com>
2024-01-05 12:25:12 +0100

    feat(experiment/rex.sh): Show success execution ratio as well

--- experiment/rex.sh
+++ experiment/rex.sh
@@ -22,12 +22,17 @@ skip_measurement='true' h 10-rex-set-via-ip.log "settings set --name remote_exec
 skip_measurement='true' a 11-rex-cleanup-know_hosts.log satellite6 -m "shell" -a "rm -rf /usr/share/foreman-proxy/.ssh/known_hosts*"
 
 skip_measurement='true' h 12-rex-date.log "job-invocation create --async --description-format 'Run %{command} (%{template_name})' --inputs command='date' --job-template '$job_template_ssh_default' --search-query 'name ~ container'"
+jsr $logs/12-rex-date.log
 j $logs/12-rex-date.log
 skip_measurement='true' h 12-rex-date-ansible.log "job-invocation create --async --description-format 'Run %{command} (%{template_name})' --inputs command='date' --job-template '$job_template_ansible_default' --search-query 'name ~ container'"
+jsr $logs/12-rex-date-ansible.log
 j $logs/12-rex-date-ansible.log
 skip_measurement='true' h 13-rex-sm-facts-update.log "job-invocation create --async --description-format 'Run %{command} (%{template_name})' --inputs command='subscription-manager facts --update' --job-template '$job_template_ssh_default' --search-query 'name ~ container'"
+jsr $logs/13-rex-sm-facts-update.log
 j $logs/13-rex-sm-facts-update.log
 skip_measurement='true' h 14-rex-uploadprofile.log "job-invocation create --async --description-format 'Run %{command} (%{template_name})' --inputs command='dnf uploadprofile --force-upload' --job-template '$job_template_ssh_default' --search-query 'name ~ container'"
+jsr $logs/14-rex-uploadprofile.log
 j $logs/14-rex-uploadprofile.log
+
 
 junit_upload
