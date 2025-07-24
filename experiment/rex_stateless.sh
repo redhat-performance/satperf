5c209680f55a28c4ef5122e0795bf9ee92c89f86
Pablo Méndez Hernández <pablomh@redhat.com>
2025-07-24 10:16:36 +0200

    feat: Add new `rex_stateless.sh` experiment
    It will run the following commands with low disk space increase
    on the content hosts:
    
    - `date`
    - `dnf uploadprofile --force-upload`
    - `sleep 300; uptime`

---
+++ experiment/rex_stateless.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+source experiment/run-library.sh
+
+branch="${PARAM_branch:-satcpt}"
+inventory="${PARAM_inventory:-conf/contperf/inventory.${branch}.ini}"
+
+opts="--forks 100 -i $inventory"
+opts_adhoc="$opts"
+
+iop_enabled="$( get_inventory_var enable_iop )"
+
+
+section 'Checking environment'
+# generic_environment_check false false
+unset skip_measurement
+set +e
+
+
+section 'Remote execution'
+# job_template_ansible_default='Run Command - Ansible Default'
+job_template_ssh_default='Run Command - Script Default'
+
+skip_measurement='true' h 10-rex-set-via-ip.log \
+  'settings set --name remote_execution_connect_by_ip --value true'
+skip_measurement='true' a 11-rex-cleanup-know_hosts.log \
+  -m 'ansible.builtin.shell' \
+  -a 'rm -rf /usr/share/foreman-proxy/.ssh/known_hosts*' \
+  satellite6
+
+test=15-rex-date
+skip_measurement=true h "${test}.log" \
+  "job-invocation create --async --description-format 'Run %{command} (%{template_name})' --inputs command='date' --job-template '$job_template_ssh_default' --search-query 'name ~ container'"
+jsr "${logs}/${test}.log" 15
+j "${logs}/${test}.log" &
+
+test=16-rex-dnf_uploadprofile
+skip_measurement=true h "${test}.log" \
+  "job-invocation create --async --description-format 'Run %{command} (%{template_name})' --inputs command='dnf uploadprofile --force-upload' --job-template '$job_template_ssh_default' --search-query 'name ~ container'"
+jsr "${logs}/${test}.log" 15
+j "${logs}/${test}.log" &
+
+test=19-rex-sleep_300_uptime
+skip_measurement=true h "${test}.log" \
+  "job-invocation create --async --description-format 'Run %{command} (%{template_name})' --inputs command='sleep 300; uptime' --job-template '$job_template_ssh_default' --search-query 'name ~ container'"
+jsr "${logs}/${test}.log" 30
+j "${logs}/${test}.log" &
+
+
+wait
+
+
+junit_upload
